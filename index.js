51d98f930386abe2d5e2f4d77bfa7b66431e707a
truittjanney <153782672+truittjanney@users.noreply.github.com>
2024-01-27 23:34:48 +0000

    project progress 1

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import 'styles.css';
+import "styles.css";
 
 window.onload = () => {
-
-    let randomNumber = Math.random() * 10;
-    document.querySelector('#the-excuse')
-    console.log('Hello Rigo from the console! '+randomNumber);
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse");
+  console.log("Hello Rigo from the console! " + randomNumber);
 };
