5ad1fded567bd1cf0f9503bba6bd6d1fff86286c
truittjanney <153782672+truittjanney@users.noreply.github.com>
2024-01-27 23:29:05 +0000

    project progress 1

---
+++ index.js
@@ -0,0 +1,8 @@
+import 'styles.css';
+
+window.onload = () => {
+
+    let randomNumber = Math.random() * 10;
+    document.querySelector('#the-excuse')
+    console.log('Hello Rigo from the console! '+randomNumber);
+};
